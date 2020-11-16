3dcbca1e92186c1f0fdf5d8abf50c1cc4274ea71
FIC <FIC@info.local>
2020-11-16 07:52:59 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu ano de nascimento :");
+            int anoDeNascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o ano atual!");
+            int anoAtual = int.Parse(Console.ReadLine());
+            int idade = (anoAtual - anoDeNascimento);
+            int semanas = (idade * 52);
+            Console.WriteLine("idade em anos :" +idade);
+            Console.WriteLine("idade em semanas :"+semanas);
+            
+        }
+    }
+}
